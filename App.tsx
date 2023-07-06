2a07c0c4f36ceca493b8c09a967c66dee4f23476
Suzette Sousa <sousa.suzette.pro@gmail.com>
2023-07-06 15:10:08 +0200

    init project

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import QuestionComponent from './src/components/QuestionComponent';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <QuestionComponent />
+    </View>
+  );
+};
+
+export default App;
